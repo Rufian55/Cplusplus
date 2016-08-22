fbe82bbbe2320cfd17dead1bc5c28d001d02d340
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-22 14:19:50 -0400

    Update Main.cpp

--- Iteration vs. Recursion/Main.cpp
+++ Iteration vs. Recursion/Main.cpp
@@ -14,12 +14,12 @@ int main() {
 	int input1;
 	int input2;
 	double accum1, accum2, accum3, accum4;
-	Recursions rec;							// Recursions object.
+	Recursions rec;					// Recursions object.
 	struct timespec requestStart;			// Start		
-	struct timespec requestEnd;				// End 
+	struct timespec requestEnd;			// End 
 
 /************************************************************************
-*			Recursion vs. Iteration with Fibonacci Numbers				*
+*		Recursion vs. Iteration with Fibonacci Numbers		*
 *************************************************************************/
 	input1 = rec.collect_1(0, 40);
 
@@ -28,12 +28,12 @@ int main() {
 	cout << "\nFinding Fibonacci sequence number '" << input1 << "'...\n"
 		<< "Calling Recursive Fibonacci implementation!" << "\n";
 
-	/* Calculate time taken by a Recursive Function Call (1) */
+	/* Calculate time taken by a Recursive Function Call. */
 	clock_gettime(CLOCK_REALTIME, &requestStart);
 	fr.PrintFibonacci(); // "Recursive"
 	clock_gettime(CLOCK_REALTIME, &requestEnd);
 
-	/* Display time taken by a Recursive Function Call (1) */
+	/* Display time taken by a Recursive Function Call. */
 	accum1 = (requestEnd.tv_sec - requestStart.tv_sec)
 		+ (requestEnd.tv_nsec - requestStart.tv_nsec)
 		/ BILLION;
@@ -43,12 +43,12 @@ int main() {
 
 	FibonacciNR fnr(input1);
 
-	/* Calculate time taken by a Recursive Function Call (1) */
+	/* Calculate time taken by a Recursive Function Call. */
 	clock_gettime(CLOCK_REALTIME, &requestStart);
 	fnr.PrintFibonacci(); // "Non Recursive (Iterative)"
 	clock_gettime(CLOCK_REALTIME, &requestEnd);
 
-	/* Display time taken by a Recursive Function Call (1) */
+	/* Display time taken by a Recursive Function Call. */
 	accum2 = (requestEnd.tv_sec - requestStart.tv_sec)
 		+ (requestEnd.tv_nsec - requestStart.tv_nsec)
 		/ BILLION;
@@ -56,13 +56,13 @@ int main() {
 
 
 /************************************************************************
-*				Tail Recursion vs. Non Tail Recursion.					*
+*		Tail Recursion vs. Non Tail Recursion.			*
 *************************************************************************/
 	input2 = rec.collect_2(0, 50);
 
 	cout << "\nCalling Non Tail Recursive Factorial Function\n";
 
-	/* Calculate time taken by a Non Tail Recursive Factorial Function Call (1) */
+	/* Calculate time taken by a Non Tail Recursive Factorial Function Call. */
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &requestStart);
 	
 	for (i = 1; i < 1000; i++) {
@@ -71,7 +71,7 @@ int main() {
 
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &requestEnd);
 
-	/* Display time taken by a Recursive Function Call (1) */
+	/* Display time taken by a Recursive Function Call. */
 	accum3 = (requestEnd.tv_sec - requestStart.tv_sec)
 		+ (requestEnd.tv_nsec - requestStart.tv_nsec);
 	std::printf("%lf\n", accum3);
@@ -80,15 +80,15 @@ int main() {
 	
 	cout << "\nCalling Tail Recursive Factorial Function\n";
 
-	/* Calculate time taken by a Tail Recursive Factorial Function Call (1) */
+	/* Calculate time taken by a Tail Recursive Factorial Function Call. */
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &requestStart);
 
 	for (i = 1; i < 1000; i++) {
-		rec.factorial(input2); // "Tail Recursive"
+		rec.factorial(input2); 	// "Tail Recursive"
 	}
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &requestEnd);
 
-	/* Display time taken by a Recursive Function Call (1) */
+	/* Display time taken by a Recursive Function Call. */
 	accum4 = (requestEnd.tv_sec - requestStart.tv_sec)
 		+ (requestEnd.tv_nsec - requestStart.tv_nsec);
 	std::printf("%lf\n", accum4);
@@ -96,9 +96,3 @@ int main() {
 	cout << "\nTail recursion function for int " << input2 << " = "
 		<< rec.factorial(input2) << "\n";
 }
-
-/*
-Citations:
-(1)	h ttp://stackoverflow.com/questions/2418157/ubuntu-linux-c-error-undefined-reference-to-clock-gettime-and-clock-settim
-(2) Also see included header files for additional citations. 
-*/

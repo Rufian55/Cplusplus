bf06c6710ef3c8b214443e67627555e313cdd93b
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-22 09:31:32 -0400

    Update BlueMen.cpp

--- Creature Combat I/BlueMen.cpp
+++ Creature Combat I/BlueMen.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************************
 ** Program Filename: BlueMen.cpp
-** Author: Chris Kearns (CS-162-400 W2016)
+** Author: Chris Kearns
 ** Date: 2/14/2016
 ** Description: BlueMen class implementation file.
 ** Input:

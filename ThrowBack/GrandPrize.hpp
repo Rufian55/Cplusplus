9af195d9e1a48c3869a651c721eeff95cb436e9b
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-22 10:59:52 -0400

    Update GrandPrize.hpp

--- ThrowBack/GrandPrize.hpp
+++ ThrowBack/GrandPrize.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************************
 ** Program Filename: GrandPrize.hpp
-** Author: Chris Kearns (CS-162-400 W2016)
+** Author: Chris Kearns
 ** Date: 3/13/2016
 ** Description: Header file for Oscillator.cpp.
 ****************************************************************************************/
@@ -13,6 +13,6 @@ private:
 protected:
 public:
 	void grandPrize();
-	virtual void boostStrength() {}	// "Special" virtual function!
+	virtual void boostStrength() {}
 };
 #endif

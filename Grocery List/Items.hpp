4f1494d6536b1fd8b200e1ab5dd190f4a57e2ec3
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-22 09:18:49 -0400

    Update Items.hpp

--- Grocery List/Items.hpp
+++ Grocery List/Items.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************************
 ** Program Filename: Items.hpp
-** Author: Chris Kearns (CS-162-400 W2016)
+** Author: Chris Kearns
 ** Date: 1/26/2016
 ** Description: Items class header file.
 ** Input:
@@ -13,24 +13,24 @@ using std::string;
 
 class Items {
 private:
-	string name;								// Items object name value.
-	string unit;								// Items object unit value. 
-	double num2Buy;								// Items object num2Buy value.
-	double unitPrice;							// Items object unitPrice value.
-	double extPrice;							// Items object extPrice calculated value. 
+	string name;
+	string unit;
+	double num2Buy;
+	double unitPrice;
+	double extPrice;
 public:
-	Items(string, string, double, double);		// Constructor.
-	Items();									// Default Constructor
-	void setName(string);						// var name setter.
-	string getName();							// var name getter.
-	void setUnit(string);						// var unit setter.
-	string getUnit();							// var unit getter.
-	void setNum2Buy(double);					// var num2Buy setter.
-	double getNum2Buy();						// var num2Buy getter.
-	void setUnitPrice(double);					// var unitPrice setter.
-	double getUnitPrice();						// var unitPrice setter.
-	void setExtPrice(double, double);			// var extPrice calculate and set.
-	double getExtPrice();						// var extPrice getter.
-	void displayItems();						// Individual Items object member vars print
-};												//   to console function.
-#endif // !ITEMS_HPP
+	Items(string, string, double, double);
+	Items();
+	void setName(string);
+	string getName();
+	void setUnit(string);
+	string getUnit();
+	void setNum2Buy(double);
+	double getNum2Buy();
+	void setUnitPrice(double);
+	double getUnitPrice();
+	void setExtPrice(double, double);
+	double getExtPrice();
+	void displayItems();
+};
+#endif

1c29fe70505ebd5f46313468e2ec903640123670
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-22 09:22:21 -0400

    Update List.hpp

--- Grocery List/List.hpp
+++ Grocery List/List.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************************
 ** Program Filename: List.hpp
-** Author: Chris Kearns (CS-162-400 W2016)
+** Author: Chris Kearns
 ** Date: 1/26/2016
 ** Description: List class header file.
 ** Input:
@@ -12,17 +12,17 @@
 
 class List {
 private:
-	double totPrice;						// List total price of all var Items::extPrice
-	int numItems;							// Number of items in list.
-	Items *aList;							// Items pointer for List's dynmamic array/
-	int sizeArray;							// sizeArray utility var.
+	double totPrice;				// List total price of all var Items::extPrice
+	int numItems;					// Number of items in list.
+	Items *aList;					// Items pointer for List's dynmamic array
+	int sizeArray;					// sizeArray utility var.
 public:
-	List();									// List default constructor.
-	~List();								// List destructor.
-	void addItem(Items);					// Function adds Items objects to List.
-	void removeItem(int);					// Function removes Items objects from List.
-	double calcTotal();						// Function to calculate and retrun totPrice var.
-	void displayCart();						// Function to print List to console.
-	friend bool operator==(string, string);	// == overload for duplicate check via Item name comparison.
+	List();						// List default constructor.
+	~List();					// List destructor.
+	void addItem(Items);				// Function adds Items objects to List.
+	void removeItem(int);				// Function removes Items objects from List.
+	double calcTotal();				// Function to calculate and retrun totPrice var.
+	void displayCart();				// Function to print List to console.
+	friend bool operator==(string, string);		// == overload for duplicate check via Item name comparison.
 };
-#endif // !LIST_HPP
+#endif

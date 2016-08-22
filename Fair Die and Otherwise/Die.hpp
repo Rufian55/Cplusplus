b69988b6b6ce129fc8b6b038a0f0830670fb5a94
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-22 12:28:43 -0400

    Update Die.hpp

--- Fair Die and Otherwise/Die.hpp
+++ Fair Die and Otherwise/Die.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************************
 ** Program Filename: Die.hpp
-** Author: Chris Kearns (CS-162-400 W2016)
+** Author: Chris Kearns
 ** Date: 1/24/2016
 ** Description: Die class header file.
 ** Input: 
@@ -12,15 +12,15 @@
 class Die{
 
 private:
-	int sides;					// Die # of sides.
-	bool load;					// Die loaded T/F
+	int sides;			// Die # of sides.
+	bool load;			// Die loaded T/F
 
 public:
 	void createDice(int, bool);	// Constructor.
-	void setSides(int);			// Sets # of sides.
-	void setLoad(bool);			// Sets loaded doe T/F.
-	int getSides();				// Returns die object's number of sides.
-	bool getLoad();				// Returns die object's loaded T/F.
-	int roll();					// Returns result of die object being rolled.
+	void setSides(int);		// Sets # of sides.
+	void setLoad(bool);		// Sets loaded doe T/F.
+	int getSides();			// Returns die object's number of sides.
+	bool getLoad();			// Returns die object's loaded T/F.
+	int roll();			// Returns result of die object being rolled.
 };
 #endif

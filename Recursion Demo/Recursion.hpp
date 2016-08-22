4dfbfb63868dfaaf302a476fe56e4c8fb48346b3
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-22 12:41:33 -0400

    Update Recursion.hpp

--- Recursion Demo/Recursion.hpp
+++ Recursion Demo/Recursion.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************************
 ** Program Filename: List.hpp
-** Author: Chris Kearns (CS-162-400 W2016)
+** Author: Chris Kearns
 ** Date: 1/26/2016
 ** Description: List class header file.
 ** Input:
@@ -17,7 +17,7 @@ private:
 public:
 	string revString(string);			// Prototype.
 	int sumArray(int [], int);			// Prototype.
-	int triNum(int);					// Prototype.
+	int triNum(int);				// Prototype.
 
 };
-#endif // !RECURSSION_HPP
+#endif

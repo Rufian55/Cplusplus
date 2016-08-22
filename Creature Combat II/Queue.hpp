8bc9bc88ad47b19a48bd3fb0c7be353691a74131
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-22 10:28:35 -0400

    Update Queue.hpp

--- Creature Combat II/Queue.hpp
+++ Creature Combat II/Queue.hpp
@@ -1,8 +1,8 @@
 /****************************************************************************************
 ** Program Filename: Queue.hpp
-** Author: Chris Kearns (CS-162-400 W2016)
+** Author: Chris Kearns
 ** Date: 2/28/2016
-** Description: Queue class declaration file adapted from Gaddis, Chapter 18.
+** Description: Queue class declaration file.
 *****************************************************************************************/
 #ifndef QUEUE_HPP
 #define QUEUE_HP
@@ -11,27 +11,27 @@
 
 class Queue : Creature {
 private:
-	class QueueNode {											// Data type of the nodes.
+	class QueueNode {						// Data type of the nodes.
 	private:
-		friend class Queue;										// Set to friend so we have access to Queue members.
-		Creature *aCreature;									// a Creature type pointer.
-		QueueNode *next;										// Declare Queuenode type pointer next.
+		friend class Queue;					// Set to friend so we have access to Queue members.
+		Creature *aCreature;					// a Creature type pointer.
+		QueueNode *next;					// Declare Queuenode type pointer next.
 		// Constructor for the QueueNodes.
 		QueueNode(Creature *aCret, QueueNode *next1 = NULL) {	// QueueNode pointer next1 = NULL as queue is initialized empty. 
-			aCreature = aCret;									// Sets the Creature pointer var in each new node to *aCret paramter.
-			next = next1;										// Sets each new node's next pointer to NULL as it's the back
-		}														// of the queue where the new QueueNode is being added.
+			aCreature = aCret;				// Sets the Creature pointer var in each new node to *aCret paramter.
+			next = next1;					// Sets each new node's next pointer to NULL as it's the back
+		}							// of the queue where the new QueueNode is being added.
 	};
 	QueueNode *front;						// QueueNode pointer to keep track of the front of the queue.
 	QueueNode *back;						// QueueNode pointer to keep track of the back of the queue.
 public:
-	Queue();								// Queue class constructor prototype.
-	~Queue();								// Queue class destructor prototype.
+	Queue();
+	~Queue();
 	
-	void add(Creature *);					// Prototype.
-	Creature* remove();						// Prototype.
-	bool isEmpty();							// Prototype.
-	void clear();							// Prototype.
+	void add(Creature *);
+	Creature* remove();
+	bool isEmpty();
+	void clear();
 
 	// Virtual function placeholders.
 	int roll(string) {};
@@ -46,4 +46,4 @@ public:
 	void setcScore(int) {};
 	int getcScore() {};
 };
-#endif // !QUEUE_HPP
+#endif

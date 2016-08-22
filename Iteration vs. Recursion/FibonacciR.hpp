b0d2efba025d98a95b80ac39a111ddb41d0073af
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-22 14:16:33 -0400

    Update FibonacciR.hpp

--- Iteration vs. Recursion/FibonacciR.hpp
+++ Iteration vs. Recursion/FibonacciR.hpp
@@ -1,9 +1,3 @@
-/* (2)
-FibonacciNR.hpp
-Created on: Sep 11, 2010
-Author: julian
-joseph.arul83@gmail.com
-*/
 #ifndef FIBONACCIR_HPP
 #define FIBONACCIR_HPP
 
@@ -12,12 +6,9 @@ private:
 	FibonacciR();
 	int Fibonacci(const int &n);
 	const int *n_;
-
 public:
 	FibonacciR(const int &n);
 	virtual ~FibonacciR();
 	void PrintFibonacci();
-
 };
-
 #endif // FIBONACCIR_HPP

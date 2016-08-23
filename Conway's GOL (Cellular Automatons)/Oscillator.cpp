4bd89b6a3b6911d4d2e1f07e1e8dcf7ae98be9b9
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-23 10:14:00 -0400

    Update Oscillator.cpp

--- Conway's GOL (Cellular Automatons)/Oscillator.cpp
+++ Conway's GOL (Cellular Automatons)/Oscillator.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************************
 ** Program Filename: Oscillator.cpp
-** Author: Chris Kearns (CS-162-400 W2016)
+** Author: Chris Kearns
 ** Date: 1/17/2016
 ** Description: Contains the oscillator() function.
 ** Input: array "collector"
@@ -24,16 +24,16 @@ using std::endl;
 ****************************************************************************************/
 void oscillator(int anArray[]) {
 	int univRows, winRows, univCols, winCols;
-	univRows = 60;									// Row bounds of our Universe.
-	winRows = 40;									// Row bounds of our Window.
-	univCols = 40;									// Column bounds of our Universe.
-	winCols = 20;									// Column bounds of our Window.
-	int iter = 0;									// Counter - 1 anim. loop is 1 iteration.
-	int time = 50000 * anArray[0];					// Time between iterations.
-	int iterations = anArray[1];					// Iterations desired by user.
-	int aliveCount = 0;								// Counter to determine cell state.
-	int xOffset = anArray[2] + 10;					// x axis offset for initial image.
-	int yOffset = anArray[3] + 10;					// y-axis offset for inital image.
+	univRows = 60;					// Row bounds of our Universe.
+	winRows = 40;					// Row bounds of our Window.
+	univCols = 40;					// Column bounds of our Universe.
+	winCols = 20;					// Column bounds of our Window.
+	int iter = 0;					// Counter - 1 anim. loop is 1 iteration.
+	int time = 50000 * anArray[0];			// Time between iterations.
+	int iterations = anArray[1];			// Iterations desired by user.
+	int aliveCount = 0;				// Counter to determine cell state.
+	int xOffset = anArray[2] + 10;			// x axis offset for initial image.
+	int yOffset = anArray[3] + 10;			// y-axis offset for inital image.
 
 	// Our initial Universe array matrix_0 - all cells dead.
 	std::string matrix_0[univRows][univCols];
@@ -45,7 +45,7 @@ void oscillator(int anArray[]) {
 		}
 	}
 
-	// "Inject" initial Galaxy image. See fotnote (6) in Main.cpp
+	// "Inject" initial Galaxy image.
 	int i, j;
 	i = j = 0;
 
@@ -100,10 +100,9 @@ void oscillator(int anArray[]) {
 			cout << endl;
 		}
 
-		// Sleep. See footnote(3) in Main.cpp.
 		usleep(time);
 
-		// Move cursor up above matrix_1 rendering. See footnote (1) in Main.cpp
+		// Move cursor up above matrix_1 rendering.
 		cout << "\033[41A" << "Jan Kok's Galaxy is running from point: (" << anArray[2]
 			 << "," << anArray[3] << "). " << "Iteration count = " << iter << endl;
 
@@ -112,7 +111,7 @@ void oscillator(int anArray[]) {
 		for (int i = 1; i < 59; i++) {
 			for (int j = 1; j < 39; j++) {
 
-				aliveCount = 0;						// Count occupied cells.
+				aliveCount = 0;				// Count occupied cells.
 
 				if (matrix_0[i - 1][j - 1] == "X")	// Top left.
 					aliveCount++;
@@ -174,7 +173,7 @@ void oscillator(int anArray[]) {
 		for (int i = 1; i < 59; i++) {
 			for (int j = 1; j < 39; j++) {
 
-				aliveCount = 0;						// Count occupied cells.
+				aliveCount = 0;				// Count occupied cells.
 
 				if (matrix_1[i - 1][j - 1] == "X")	// Top left.
 					aliveCount++;
@@ -223,13 +222,11 @@ void oscillator(int anArray[]) {
 			}
 		}
 
-		// Sleep. See footnote(3) in Main.cpp.
 		usleep(time);
 
 		iter++;
 
 		// Move cursor up above matrix_1 rendering, clear console, user message!
-		// See footnote (1) in Main.cpp
 		cout << "\033[41A" << "Jan Kok's Galaxy is running from point: (" << anArray[2]
 			 << "," << anArray[3] << "). " << "Iteration count = " << iter << endl;
 
